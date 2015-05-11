d0b9311c3e58feda16cedbbceb23bf24a0eb2bd1
jsimmons--dev <jsimmons@udel.edu>
2015-05-10 23:22:54 -0400

    implemented reading, printing, extracting

--- main.cpp
+++ main.cpp
@@ -1,32 +1,81 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#define INPUT_ROWS 20
 using namespace std;
 
+struct matchTreeNode {
+struct matchTreeNode *child;
+struct matchTreeNode *parent;
+};
 
-void readRows(vector<vector<string> > &rows)
+
+void readIn(string (&rows)[INPUT_ROWS])
 {
-    int i = 0;
     string current = " ";
-    while(current.compare("END") != 0)
+    for(size_t i = 0;i<INPUT_ROWS && current.compare("END") != 0; ++i)
     {
-        //vector<vector<string> > currentProblem;
-        //rows.insert(i,currentProblem);
-        while(current.compare("") != 0)
-        {
-        rows.at(i).push_back(current);
         getline(cin,current);
-        }
-        i++;
+        rows[i] = current;
     }
+}
+void printInput(string (&rows)[INPUT_ROWS])
+{
+    for(size_t i = 0;i<INPUT_ROWS;++i)
+    {
+    cout << rows[i]  << endl;
+    }
+}
+
+vector<string> extract(string (&rows)[INPUT_ROWS],size_t &start)
+{
+    vector<string> problem;
+    string current = " ";
+    for(size_t i = start; i < INPUT_ROWS && rows[i] != ""; ++i)
+    {
+        problem.push_back(rows[i]);
+        start++;
+    }
+    start++;
+    return problem;
+}
+
+vector<string> printProb(vector<string> problem)
+{
+    for(vector<string>::iterator it = problem.begin();it != problem.end();++it)
+    {
+        cout << *it << endl;
+    }
+    cout << endl;
+    return problem;
+}
+
+
+bool ending(vector<string> problem)
+{
+    return (problem.at(0).compare("END") == 0) ? true : false;
+}
+
+vector<string> solve(vector<string> problem)
+{
+    for(vector<string>::iterator it = problem.begin(); it != problem.end();++it)
+    {
+        if(it == problem.begin()) it = problem.erase(it);
+        string line = *it;
+        if(line.compare("END") == 0) it = problem.erase(it);
+        *it = "solved";
+    }
+    return problem;
 }
 
 int main()
 {
-    vector<vector<string> > rows;
-    readRows(rows);
-    //for(vector<string>::const_iterator i = rows.begin(); i != rows.end();++i)
-    //{
-    //    cout<<*i<<endl;
-    //}
+    string rows[INPUT_ROWS];
+    readIn(rows);
+    size_t pos = 0;
+    do
+    {
+        printProb(solve(extract(rows,pos)));
+    } while (!ending(printProb(solve(extract(rows,pos)))));
+
 }
--- pseudo-main.cpp
+++ pseudo-main.cpp
@@ -1,7 +1,6 @@
 /*
  * This class outlines the algorithms used for project 2
  */
-
 int main(){
     //this vector holds elements which are vectors of each problem to optimize
     //each element is a vector of lines of the problem
@@ -18,8 +17,80 @@ int main(){
     //vector
     findMatches(matchTrees,problemSet);
 
+    foreach(vector<MatchTree> problemMatches : matchTrees)
+    {
+        foreach(MatchTree tree : problemMathes)
+   
+        {
+       
+            rightMost = findRightMost(tree);
+       
+            match = shift(rightMost);
+        
+            getOptimizationNumbers(match);
+    
 
-
+  
+            solution;
+   
+            paraWidthTies = getparaWidthTies(matchTrees);
 
     
+            if(resolveparaTies(paraWidthTies) != empty)
+    
+            {
+        
+                spaceInsTies = getspaceInsTies(paraWidthTies);
+        
+                if(resolveInsTies(spaceInsTies) != empty)
+                {
+                    topTies = getTopTies(spaceInsTies);
+                     if(resolveTopTies(topTies) != empty)
+                     {
+                        solution = resolveLeftTies(topTies);
+                     } else{solution = topTies}
+                } else{solution = spaceInsTies}
+            } else{solution = paraWidthTies}
+       
+       printSolution();
+
+       }
+    }
+}   
+
+void findMatches(vector<vector<MatchTree> > matchTrees,
+        vector<vector<string> > problemSet)
+{
+    int problemIndex;
+    foreach(vector<string> problem : problemSet)
+    {
+        int lineIndex;
+        target = problem.pop();
+       foreach(string line : problem)
+       {
+           int charIndex;
+            foreach(char c : line)
+            {
+                if(c == target.0)
+                {
+                matchTrees.get(i).push(new MatchTree(charIndex,lineIndex);
+                }
+                foreach(MatchTree match : matchTrees.get(problemIndex);
+                {
+                    if(match.needed() == c)
+                    {
+                        match.point(match.levelUp,c);
+                    }
+                }
+                if(c == '\n')
+                {
+                //removes if match was found to be invalid
+                match.validate();
+                }
+                charIndex++;
+            }
+            lineIndex++;
+       }
+       problemIndex++;
+    }
 }
